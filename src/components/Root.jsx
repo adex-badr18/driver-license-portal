b39bb49d91f00303c263f2dd07a1d3c3537ee5e5
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-07-28 22:35:04 +0100

    refactor: Modify general loading state

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,30 +1,31 @@
-
-import { Outlet, useNavigation } from 'react-router-dom'
-import Header from './Header'
-import Footer from './Footer'
-import NavBar from './NavBar'
-import Spinner from './Spinner'
+import { Outlet, useNavigation } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import Spinner from "./Spinner";
+import ReusableModal from "./ReusableModal";
 
 const Root = () => {
-  const { state } = useNavigation();
+    const { state } = useNavigation();
+    const isLoading = state === "loading";
 
-  return (
-    <div className='grid grid-rows-[auto_1fr_auto] min-h-screen'>
-      <header className=''>
-        <Header />
-        <NavBar />
-      </header>
+    return (
+        <div className="grid grid-rows-[auto_1fr_auto] max-w-[100svw] overflow-hidden relative min-h-screen">
+            <header>
+                <Header />
+                <NavBar />
+            </header>
 
+            <main>
+                <Outlet />
+                <ReusableModal isOpen={isLoading}>
+                    <Spinner />
+                </ReusableModal>
+            </main>
+            
+            <Footer />
+        </div>
+    );
+};
 
-      <main className=''>
-        {
-          state === 'loading' ? <Spinner /> : <Outlet />
-        }
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
-
-export default Root
+export default Root;
