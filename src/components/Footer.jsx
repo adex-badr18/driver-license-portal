90ed2ae0e6fc83ec4b8badd7f3799a7b236ead9d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-07-28 21:23:14 +0100

    refactor: Modify footer page design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,119 +1,123 @@
 import { Link } from "react-router-dom";
-import facebook from "../assets/facebook.svg";
-import instagram from "../assets/instagram.svg";
-import twitter from "../assets/twitter.svg";
+import { FaTwitter } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
 
-import logo from "../assets/images/dannon-logo2.png"
-import youtube from "../assets/youtube.svg";
+import logo from "../assets/Logo2.svg";
 import angleright from "../assets/angle-right.svg";
-
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
-
-
     return (
         <footer className="print:hidden">
-            <div className="flex lg:flex-row flex-col gap-10 justify-between px-4 py-8 md:px-20 md:pt-16 md:pb-12 bg-neutral-900">
-
-                <div className="bg-whit">
-                    <img src={logo} alt="" className="-mb-5" />
-                    <span className="text-[11px] tracking-tight text-custom-green uppercase font-bold ml-2">
-                        Instant Drivers License
-                    </span>
-
-                    {/* <p className="text-white text-xl md:w-1/2 ">Get your Driver license done anywhere in the world in an instant</p> */}
+            <div className="flex flex-wrap lg:flex-row flex-col gap-10 md:gap-14 justify-between px-4 py-8 md:px-10 md:pt-16 md:pb-10 bg-black">
+                <div className="grid gap-8 content-start ">
+                    <img className="h-12 md:h-14" src={logo} alt="" />
+                    <p className="text-white text-sm leading-[1.8] md:max-w-[20rem]">
+                        Fast, secure, and efficient licensing solutions at your
+                        fingertips. <br /> For customer support, please reach
+                        out to{" "}
+                        <span className="text-green-500">
+                            support@example.com.
+                        </span>{" "}
+                    </p>
                 </div>
 
                 <div>
-                    <p className="font-bold mb-4 text-green-500">Quick Links</p>
+                    <p className="font-bold mb-4 text-green-600">Quick Links</p>
                     <ul className="grid gap-2">
                         <li>
-                            <Link className="flex text-gray-50 gap-2 hover:text-green-500">
-                                <img className="w-2 " src={angleright} alt="" />{" "}
+                            <Link
+                                to={"/application"}
+                                state={"new"}
+                                className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-50 gap-2"
+                            >
                                 New Drivers License
                             </Link>
                         </li>
 
                         <li>
-                            <Link className="flex text-gray-50 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} /> Drivers
-                                License Renewal
+                            <Link
+                                to={"/application"}
+                                state="renewal"
+                                className="flex font-semibold hover:translate-x-2 transition-all duration-200 hover:text-green-600 text-gray-50 gap-2"
+                            >
+                                Drivers License Renewal
                             </Link>
                         </li>
                         <li>
-                            <Link className="flex  text-gray-100 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} /> FAQ
+                            <Link
+                                to="/faq"
+                                className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-100 gap-2"
+                            >
+                                FAQ
                             </Link>
                         </li>
                         <li>
-                            <Link className="flex text-gray-200 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} /> Contact
+                            <Link
+                                to={"/support"}
+                                className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-200 gap-2"
+                            >
+                                Contact
                             </Link>
                         </li>
                         <li>
-                            <Link className="flex text-gray-100 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} />{" "}
-                                Aknowledgement Slip
+                            <Link
+                                to={"appointment"}
+                                className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-100 gap-2"
+                            >
+                                Appointment Slip
                             </Link>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <p className="font-bold mb-4 text-green-500">Other Links</p>
+                    <p className="font-bold mb-4 text-green-600">Other Links</p>
                     <ul className="grid gap-2">
                         <li>
-                            <Link className="flex text-gray-50 gap-2 hover:text-green-500">
-                                <img className="w-2 " src={angleright} alt="" />{" "}
-                                Contact Us
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link className="flex text-gray-200 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} /> Forms
+                            <Link className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-200 gap-2">
+                                Forms
                             </Link>
                         </li>
                         <li>
-                            <Link className="flex text-gray-50 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} />{" "}
+                            <Link className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-50 gap-2">
                                 Condition of use
                             </Link>
                         </li>
                         <li>
-                            <Link className="flex text-gray-100 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} /> News &
+                            <Link className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-100 gap-2">
                                 Media
                             </Link>
                         </li>
                         <li>
-                            <Link className="flex  text-gray-100 gap-2 hover:text-green-500">
-                                <img className="w-2" src={angleright} /> Privacy
-                                and Security Statement
+                            <Link className="flex font-semibold hover:text-green-600 hover:translate-x-2 transition-all duration-200 text-gray-100 gap-2">
+                                Privacy and Security Statement
                             </Link>
                         </li>
                     </ul>
                 </div>
 
                 <div>
-                    <p className="mb-4 font-bold text-green-500">Follow Us:</p>
-                    <ul className="flex items-center gap-4">
-                        <li>
-                            <img className="w-8 cursor-pointer" src={facebook} alt="" />
+                    <p className="font-bold mb-4 text-green-600">Socials</p>
+
+                    <ul className="flex items-center text-3xl text-white gap-4">
+                        <li className="font-semibold hover:text-green-600 hover:-translate-y-2 transition-all duration-200 cursor-pointer">
+                            <FaFacebook />
                         </li>
-                        <li>
-                            <img className="w-8 cursor-pointer" src={instagram} alt="" />
+                        <li className="font-semibold hover:text-green-600 hover:-translate-y-2 transition-all duration-200 cursor-pointer">
+                            <FaInstagramSquare />
                         </li>
-                        <li>
-                            <img className="w-8 cursor-pointer" src={twitter} alt="" />
+                        <li className="font-semibold hover:text-green-600 hover:-translate-y-2 transition-all duration-200 cursor-pointer">
+                            <FaTwitter />
                         </li>
-                        <li>
-                            <img className="w-8 cursor-pointer" src={youtube} alt="" />
+                        <li className="font-semibold hover:text-green-600 hover:-translate-y-2 transition-all duration-200 cursor-pointer">
+                            <IoLogoYoutube />
                         </li>
                     </ul>
                 </div>
             </div>
-            <div className="text-center py-4 border-t border-neutral-600 grid content-center bg-neutral-900 text-white">
-                Copyright &copy; ACME 2024
+            <div className="text-center text-sm h-10 border-t grid content-center bg-black text-white">
+                All rights reserved &copy; DANNON GROUP
             </div>
         </footer>
     );
